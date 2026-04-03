aa9c09acdf3ad65b82dd2897882fe0c1302dea12
Samyak <samyakgedam03@gmail.com>
2026-04-04 00:48:23 +0530

    Practiced LinkedList

--- 1.Basics/ArrListExample.java
+++ 1.Basics/ArrListExample.java
@@ -1,10 +1,21 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+
 
 public class ArrListExample {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<List<Integer>> arr = new ArrayList<>();
+
+
+        int[] nums = {2,3,4};
+
+        arr.add(Arrays.asList(nums[0], nums[1], nums[2]));
+        arr.add(Arrays.asList(nums[0], nums[1], nums[2]));
+
         
         System.out.println(arr);
         in.close();
