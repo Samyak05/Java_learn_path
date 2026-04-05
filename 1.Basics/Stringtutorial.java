970da19f466dd100acf0bc0397e1246f1a76f28b
Samyak <samyakgedam03@gmail.com>
2026-04-06 00:46:19 +0530

    Practice string questions

--- 1.Basics/Stringtutorial.java
+++ 1.Basics/Stringtutorial.java
@@ -12,11 +12,25 @@ public class Stringtutorial {
         System.out.println(Arrays.toString(arr));
 
 
-
         // System.out.println(name);
         // System.out.println(Arrays.toString(name.split("\\s+")));
         // System.out.println(Arrays.toString(words));
         // System.out.println(words);
         // System.out.println(String.join(" ", words));
+
+
+
+        
+        // StringBuilder SB = new StringBuilder();
+
+        // SB.append('a');
+        // SB.append('b');
+        // SB.append('c');
+        // SB.append(' ');
+
+        // SB.append('e');
+        // System.out.println(SB);
+        // System.out.println(SB.toString());
+        // System.out.println(SB.reverse());
     }
 }
